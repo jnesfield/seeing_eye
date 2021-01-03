3886be3e2dfaec5d616094c520441ea06e7c62d2
jnesfield <jamesnesfield@live.com>
2021-01-02 17:41:08 -0700

    Add files via upload

--- utils/utils.py
+++ utils/utils.py
@@ -188,6 +188,11 @@ def textDetection(image, roi):
     x  = int(roi[0])
     x2 = int(roi[0]+roi[2]+1)
     
+    if y == 0:
+        y = 1
+    if x == 0:
+        x = 1
+    
     image = image[y:y2, x:x2]
     orig = image.copy()
     (H, W) = image.shape[:2]
