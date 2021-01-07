7ac57fd4e8111b128924124db037d198559c3663
jnesfield <jamesnesfield@live.com>
2021-01-06 18:33:07 -0700

    Add files via upload

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash 
+
 pactl set-default-sink 'alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo'
 pactl -- set-sink-volume 0 90%
 python3 ~/seeing_eye/main.py
