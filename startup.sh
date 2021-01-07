4fa34b37722ac9cb202cc7272d860e2049a1a0f3
jnesfield <jamesnesfield@live.com>
2021-01-07 08:54:07 -0700

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sleep 60
-pactl set-default-sink 'alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Device-00.analog-stereo'
-sleep 30
-pactl -- set-sink-volume 0 90%
-sleep 30
-python3 ~/seeing_eye/main.py
