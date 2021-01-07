e81b3dd49fc6024cd98606cc620299df71949880
jnesfield <jamesnesfield@live.com>
2021-01-07 09:45:09 -0700

    Add files via upload

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ pactl set-default-sink 'alsa_output.usb-C-Media_Electronics_Inc._USB_Audio_Devic
 sleep 30
 pactl -- set-sink-volume 0 90%
 sleep 30
-python3 ~/seeing_eye/main.py &
+python3 ~/seeing_eye/main.py
