8843e1185145f23c28bd6021394381efb0a24cf4
jnesfield <jamesnesfield@live.com>
2021-01-07 12:39:22 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,23 +2,36 @@
 import Jetson.GPIO as GPIO
 import cv2
 from utils import utils
-
+import signal
 
 #sets pins to use native numbers
 GPIO.setmode(GPIO.BOARD)
 inpin = 15
+outpin = 23
 GPIO.setup(inpin,GPIO.IN)
-x = GPIO.input(15)
+GPIO.setup(outpin,GPIO.OUT)
 
-
+def keyboardInterruptHandler(signal, frame):
+    print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal))
+    GPIO.output(outpin,0)
+    exit(0)
+    
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
+    
 #start Infinite loop to look for gpio header button to capture image, obj detect, east(text detect) and ocr(tesseract):
 while True:
     x = GPIO.input(inpin)
+    #lights up when ready
+    GPIO.output(outpin,1)
+
+        
     k = cv2.waitKey(1) & 0xFF
     # press 'q' to exit (if running diagnostics)
     if k == ord('q'):
+        GPIO.output(outpin,0)
         break
     if x == 0:
+        GPIO.output(outpin,0)
        
         #capture image
         image = utils.imageCap()
